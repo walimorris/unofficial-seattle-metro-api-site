6b554ff13ee0b93929162ee48555312ff18f1b17
Wali Morris <walimmorris@gmail.com>
2022-10-24 15:38:17 -0700

    add a cognitoUserClone on the original read only congnito-user

--- src/components/ConfirmRegistrationComponent.vue
+++ src/components/ConfirmRegistrationComponent.vue
@@ -7,6 +7,8 @@
 
 <script>
 import { CognitoUser } from 'amazon-cognito-identity-js';
+import * as AmazonCognitoIdentity from 'amazon-cognito-identity-js';
+import config from '../../config/config';
 
 export default {
   name: 'ConfirmationRegistrationComponent',
@@ -18,7 +20,32 @@ export default {
     return {
       verificationCode: null,
       confirmationResult: null, // possibly needed to re-verify a user
+      cognitoUserClone: null,
+      authenticationDetails: null,
+      userPool: null,
+      verified: false,
     };
+  },
+  created() {
+    const authenticationData = {
+      Username: this.cognitoUser.username,
+      Password: this.cognitoUser.password,
+    };
+
+    const poolData = {
+      UserPoolId: config.cognito.userPoolId,
+      ClientId: config.cognito.clientId,
+    };
+
+    // eslint-disable-next-line max-len
+    this.authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+    this.userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+    const userData = {
+      Username: this.username,
+      Pool: this.userPool,
+    };
+    this.cognitoUserClone = new CognitoUser(userData);
   },
 
   methods: {
@@ -27,14 +54,21 @@ export default {
       console.log('inside verifyUser()');
       this.verificationCode = document.getElementById('verificationCode').value;
       if (this.verificationCode !== null) {
-        this.cognitoUser.confirmRegistration(this.verificationCode, true, (error, result) => {
-          if (error) {
-            throw new Error(`Error verifying code: ${this.verificationCode}`);
-          }
-          this.confirmationResult = result;
+        if (this.cognitoUserClone !== null) {
+          // eslint-disable-next-line max-len
+          this.cognitoUserClone.confirmRegistration(this.verificationCode, true, (error, result) => {
+            if (error) {
+              // eslint-disable-next-line no-console
+              console.log(`Error verifying code: ${this.verificationCode}, verifying..`);
+            }
+            this.confirmationResult = result;
+            // eslint-disable-next-line no-console
+            console.log(`${this.username} is a registered and verified: ${this.result.text}`);
+          });
+        } else {
           // eslint-disable-next-line no-console
-          console.log(`${this.username} is a registered and verified: ${this.result.text}`);
-        });
+          console.log('CognitoUser is null');
+        }
       } else {
         // eslint-disable-next-line no-console
         console.log('verification-code is null');
