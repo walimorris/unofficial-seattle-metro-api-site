f759760b4c7d56e9a8165a2fb19bdd812f0393e6
Wali Morris <walimmorris@gmail.com>
2022-10-24 11:36:15 -0700

    complete initial register process with amazon cognito identity

--- src/components/RegisterComponent.vue
+++ src/components/RegisterComponent.vue
@@ -2,7 +2,7 @@
   <div class="register-component">
     <h1 class="register-component-title" id="title-header">Register an Account</h1>
 
-    <input type="personalname" class="form-control" id="personalnameRegister" placeholder="Name">
+    <input type="text" class="form-control" id="personalnameRegister" placeholder="Name">
     <input type="email" class="form-control" id="emailInputRegister" placeholder="Email">
     <input type="password" class="form-control" id="passwordInputRegister" placeholder="Password">
     <input type="password" class="form-control" id="confirmpass" placeholder="Confirm Password">
@@ -11,6 +11,9 @@
 </template>
 
 <script>
+import * as AmazonCognitoIdentity from 'amazon-cognito-identity-js';
+import config from '../../config/config';
+
 export default {
   name: 'RegisterComponent',
   data() {
@@ -19,6 +22,7 @@ export default {
       password: null,
       personalName: null,
       poolData: null,
+      cognitoUser: null,
     };
   },
 
@@ -34,16 +38,12 @@ export default {
     const amazonSdkScript = document.createElement('script');
     amazonSdkScript.setAttribute('src', 'https://sdk.amazonaws.com/js/aws-sdk-2.7.16.min.js');
 
-    const amazonCognitoIdentityScript = document.createElement('script');
-    amazonCognitoIdentityScript.setAttribute('src', 'js/amazon-cognito-identity.min.js');
-
     const configScript = document.createElement('script');
     configScript.setAttribute('src', 'config.js');
 
     document.head.appendChild(jqueryScript);
     document.head.appendChild(amazonCognitoAuthScript);
     document.head.appendChild(amazonSdkScript);
-    document.head.appendChild(amazonCognitoIdentityScript);
     document.head.appendChild(configScript);
   },
 
@@ -67,15 +67,41 @@ export default {
       } else {
         this.password = document.getElementById('passwordInputRegister').value;
       }
-
       this.poolData = {
-        // eslint-disable-next-line no-undef
-        UserPoolId: _config.cognito.userPoolId,
-        // eslint-disable-next-line no-undef
-        ClientId: _config.cognito.clientId,
+        UserPoolId: config.cognito.userPoolId,
+        ClientId: config.cognito.clientId,
       };
+      // eslint-disable-next-line no-unused-vars
+      const userPool = new AmazonCognitoIdentity.CognitoUserPool(this.poolData);
+      // eslint-disable-next-line no-unused-vars
+      const attributeList = [];
+      // eslint-disable-next-line no-unused-vars
+      const dataEmail = {
+        Name: 'email',
+        Value: this.username,
+      };
+      // eslint-disable-next-line no-unused-vars
+      const dataPersonalName = {
+        Name: 'name',
+        Value: this.personalName,
+      };
+      const attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail);
+      // eslint-disable-next-line max-len
+      const attributePersonalName = new AmazonCognitoIdentity.CognitoUserAttribute(dataPersonalName);
+      attributeList.push(attributeEmail);
+      attributeList.push(attributePersonalName);
 
-      // let userPool = new AmazonCognitoIdentity.CognitoUserPool(this.poolData);
+      // sign user up through user pool
+      userPool.signUp(this.username, this.password, attributeList, null, (error, result) => {
+        if (error) {
+          // eslint-disable-next-line no-console
+          console.log(error.message || JSON.stringify(error));
+        }
+        this.cognitoUser = result.user;
+        // eslint-disable-next-line no-console
+        console.log(`user name: ${this.cognitoUser.username}`);
+        document.getElementById('title-header').innerHTML = 'Check your email for verification';
+      });
     },
   },
 };
