62b7011f1d6c93cf69709370828bc83ca89ab039
Wali Morris <walimmorris@gmail.com>
2022-10-24 18:29:16 -0700

    complete ConfirmRegistration functionality

--- src/components/ConfirmRegistrationComponent.vue
+++ src/components/ConfirmRegistrationComponent.vue
@@ -1,6 +1,6 @@
 <template>
 <div id="confirm-registration">
-  <input type="text" class="form-control" id="verificationCode" placeholder="Enter Code">
+  <input type="password" class="form-control" id="verificationCode" placeholder="Enter Code">
   <button type="button" class="verify-button" v-on:click="verifyUser()">Verify</button>
 </div>
 </template>
@@ -20,59 +20,74 @@ export default {
     return {
       verificationCode: null,
       confirmationResult: null, // possibly needed to re-verify a user
-      cognitoUserClone: null,
       authenticationDetails: null,
       userPool: null,
+      userData: null,
       verified: false,
+      cognitoUserClone: null,
     };
   },
   created() {
-    const authenticationData = {
-      Username: this.cognitoUser.username,
-      Password: this.cognitoUser.password,
-    };
-
     const poolData = {
       UserPoolId: config.cognito.userPoolId,
       ClientId: config.cognito.clientId,
     };
-
-    // eslint-disable-next-line max-len
-    this.authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
     this.userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 
     const userData = {
       Username: this.username,
       Pool: this.userPool,
     };
-    this.cognitoUserClone = new CognitoUser(userData);
+    this.cognitoUserClone = new AmazonCognitoIdentity.CognitoUser(userData);
   },
 
   methods: {
+
+    /**
+     * Verify a user with a given verification code after user registration.
+     * @see CognitoUser
+     */
     verifyUser() {
-      // eslint-disable-next-line no-console
-      console.log('inside verifyUser()');
       this.verificationCode = document.getElementById('verificationCode').value;
       if (this.verificationCode !== null) {
         if (this.cognitoUserClone !== null) {
-          // eslint-disable-next-line max-len
-          this.cognitoUserClone.confirmRegistration(this.verificationCode, true, (error, result) => {
-            if (error) {
-              // eslint-disable-next-line no-console
-              console.log(`Error verifying code: ${this.verificationCode}, verifying..`);
-            }
-            this.confirmationResult = result;
-            // eslint-disable-next-line no-console
-            console.log(`${this.username} is a registered and verified: ${this.result.text}`);
-          });
-        } else {
-          // eslint-disable-next-line no-console
-          console.log('CognitoUser is null');
+          this.confirmMetroRegistration(this.cognitoUserClone);
         }
       } else {
         // eslint-disable-next-line no-console
         console.log('verification-code is null');
       }
+    },
+
+    /**
+     * Confirm's user registration for Cognito Identity Management.
+     * @param cognitoUser {CognitoUser}
+     * @returns {Promise<void>}
+     */
+    async confirmMetroRegistration(cognitoUser) {
+      document.getElementById('title-header').innerHTML = 'verifying...';
+      cognitoUser.confirmRegistration(this.verificationCode, true, (error, result) => {
+        if (error) {
+          // eslint-disable-next-line no-console
+          console.log(`Error verifying code: ${this.verificationCode}, verifying..`);
+        } else {
+          this.verified = true;
+          this.confirmationResult = result;
+        }
+      });
+      await (this.sleep(3000));
+      if (this.verified === true) {
+        document.getElementById('title-header').innerHTML = 'Verified!';
+      }
+    },
+
+    /**
+     * Conducts a timeout in milliseconds.
+     * @param ms time in ms
+     * @returns {Promise<unknown>}
+     */
+    sleep(ms) {
+      return new Promise((resolve) => setTimeout(resolve, ms));
     },
   },
 };
--- src/components/RegisterComponent.vue
+++ src/components/RegisterComponent.vue
@@ -34,24 +34,14 @@ export default {
   },
 
   mounted() {
-    // eslint-disable-next-line no-console
-    console.log('Register Component is mounted');
-    const jqueryScript = document.createElement('script');
-    jqueryScript.setAttribute('src', 'https://code.jquery.com/jquery-1.11.3.min.js');
-
     const amazonCognitoAuthScript = document.createElement('script');
     amazonCognitoAuthScript.setAttribute('src', 'js/amazon-cognito-auth.min.js');
 
     const amazonSdkScript = document.createElement('script');
     amazonSdkScript.setAttribute('src', 'https://sdk.amazonaws.com/js/aws-sdk-2.7.16.min.js');
 
-    const configScript = document.createElement('script');
-    configScript.setAttribute('src', 'config.js');
-
-    document.head.appendChild(jqueryScript);
     document.head.appendChild(amazonCognitoAuthScript);
     document.head.appendChild(amazonSdkScript);
-    document.head.appendChild(configScript);
   },
 
   created() {
@@ -60,6 +50,16 @@ export default {
   },
 
   methods: {
+
+    /**
+     * Register a user based on input from registration form. A Cognito User-Pool is
+     * instantiated with user-pool-id and client-id. An attribute list is created to
+     * push required user profile attributes. These attributes, along with given
+     * form attributes will act as parameters to the user-pool signup function.
+     *
+     * @see AmazonCognitoIdentity
+     * @link https://aws.amazon.com/blogs/mobile/accessing-your-user-pools-using-the-amazon-cognito-identity-sdk-for-javascript/
+     */
     registerUser() {
       // eslint-disable-next-line no-console
       console.log('inside registeruser()');
