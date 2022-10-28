8350906887eff6dd1e850bc3840a733109faa686
Wali Morris <walimmorris@gmail.com>
2022-10-27 21:24:38 -0700

    add a plugin that consists of application constants, refactor resend verification component to better utilize object orientation for document elements

--- src/components/ResendVerificationComponent.vue
+++ src/components/ResendVerificationComponent.vue
@@ -1,14 +1,23 @@
 <template>
   <div id="resend-verification">
-    <h1 v-show="showResendVerificationCodeForm" id="resend-verification-header">Resend Verification Form</h1>
+    <h1 v-show="showResendVerificationCodeForm" id="resend-verification-header">Resend Verification Form
+    </h1>
     <form v-show="showResendVerificationCodeForm">
       <input type="email" class="form-control" id="user-email" placeholder="email">
-      <button type="button" class="verify-button" v-on:click="resendVerificationCode()">Send Code</button>
+      <button type="button" class="verify-button"
+              id="verify-button"
+              v-on:click="resendVerificationCode()">Send Code
+      </button>
     </form>
-    <button v-show="showSignInButton" type="button" class="load-sign-in-button" v-on:click="loadSignInForm()">Sign in</button>
+    <button v-show="showSignInButton" type="button"
+            id="load-sign-in-button"
+            class="load-sign-in-button"
+            v-on:click="loadSignInForm()">Sign in
+    </button>
     <h3 id="resend-verification-message"></h3>
     <div v-if="showConfirmationRegistrationForm">
-      <confirmation-registration-component :username="username" :cognitoUser="cognitoUser"
+      <confirmation-registration-component :username="username"
+                                           :cognitoUser="cognitoUser"
                                            :showConfirmationRegistrationForm="showConfirmationRegistrationForm"/>
     </div>
   </div>
@@ -20,6 +29,7 @@ import * as AmazonCognitoIdentity from 'amazon-cognito-identity-js';
 import { Auth } from 'aws-amplify';
 import ConfirmationRegistrationComponent from '@/components/ConfirmRegistrationComponent.vue';
 import config from '../../config/config';
+import Constants from '../../plugins/Constants.js';
 
 export default {
   name: 'ResendVerificationComponent',
@@ -37,14 +47,37 @@ export default {
       showResendVerificationMessage: false,
       showConfirmationRegistrationForm: false,
       codeDeliveryDetails: null,
+
+      userNameElement: null,
+      verifyButtonElement: null,
+      signInButtonElement: null,
+      resendVerificationHeaderElement: null,
+      resendVerificationMessageElement: null,
     };
+  },
+
+  mounted() {
+    // when mounted assign document elements
+    this.userNameElement = document.getElementById('user-email');
+    this.verifyButtonElement = document.getElementById('verify-button');
+    this.signInButtonElement = document.getElementById('load-sign-in-button');
+    this.resendVerificationHeaderElement = document.getElementById('resend-verification-header');
+    this.resendVerificationMessageElement = document.getElementById('resend-verification-message');
   },
 
   methods: {
 
+    /**
+     * Resends verification code to a valid user's email address. Once a resent validation code has
+     * sent, the user is redirected to the confirmation registration component that allows the user
+     * to input the code. User's has three tries until they are redirected to sign-in.
+     *
+     * @see AmazonCognitoIdentity
+     * @see CognitoUser
+     */
     resendVerificationCode() {
-      this.username = document.getElementById('user-email').value;
-      if (this.username !== "" && this.isValidUserName(this.username)) {
+      this.username = this.userNameElement.value;
+      if (this.username !== Constants.EMPTY_STRING && this.isValidUserName(this.username)) {
         if (this.userExists(this.username)) {
           this.poolData = { UserPoolId: config.cognito.userPoolId, ClientId: config.cognito.clientId, };
           this.userPool = new AmazonCognitoIdentity.CognitoUserPool(this.poolData);
@@ -54,26 +87,37 @@ export default {
           this.cognitoUser.resendConfirmationCode((error, result) => {
             if (error) {
               console.log(`Error: ${error}`);
-              document.getElementById('resend-verification-message').innerHTML = `${error}`;
-              if (document.getElementById('user-email').value !== null)
-                document.getElementById('user-email').value = '';
+              this.resendVerificationMessageElement.innerHTML = `${error}`;
+              if (this.userNameElement.value !== null)
+                this.userNameElement.value = Constants.EMPTY_STRING;
             } else {
-              document.getElementById('resend-verification-message').innerHTML = 'Check Email for Code';
+              this.resendVerificationMessageElement.innerHTML = Constants.CODE_CHECK_EMAIL;
               this.codeDeliveryDetails = result;
               console.log(result);
               this.loadConfirmRegistrationForm();
             }
           });
         } else {
-          document.getElementById('user-email').value = '';
-          document.getElementById('resend-verification-message').innerHTML = 'User does not exists!';
+          this.userNameElement.value = '';
+          this.resendVerificationMessageElement.innerHTML = Constants.NONEXISTENT_USER;
         }
       } else {
-        document.getElementById('user-email').value = '';
-        document.getElementById('resend-verification-message').innerHTML = 'Not a valid username';
+        this.userNameElement.value = '';
+        this.resendVerificationMessageElement.innerHTML = Constants.INVALID_USERNAME;
       }
     },
 
+    /**
+     * Executes the sign-in function of {@link Auth}. If a username is recognized, with improper
+     * password passed, this function forces an error. A user can be recognized, depending on the
+     * exact exception thrown. If a user is recognized than the user exists, otherwise the user is
+     * not recognized and does not exist.
+     *
+     * @param username user sign-in username
+     * @see Auth
+     *
+     * @returns {Promise<boolean>}
+     */
     async userExists(username) {
       return await Auth.signIn(username.toLowerCase(), '123').then(response => {
         return false;
@@ -95,6 +139,13 @@ export default {
       })
     },
 
+    /**
+     * Matches a username to a correct email given a regex pattern. Truthy if the email is valid,
+     * false otherwise.
+     *
+     * @param username sign-in username
+     * @returns {boolean}
+     */
     isValidUserName(username) {
       const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
       const isValid = new RegExp(emailRegex).test(username.toLowerCase());
@@ -102,6 +153,9 @@ export default {
       return isValid;
     },
 
+    /**
+     * Removes ResendVerification elements and loads the Sign In interface.
+     */
     loadSignInForm() {
       if (this.showResendVerificationMessage === true) {
         this.showResendVerificationMessage = false;
@@ -109,17 +163,21 @@ export default {
       window.location.reload();
     },
 
+    /**
+     * Loads the Confirmation Registration form that allows users to input verification codes.
+     */
     loadConfirmRegistrationForm() {
       this.removeResendVerificationFeatures();
       this.showConfirmationRegistrationForm = true;
-      console.log('Show confirmation registration form' + this.showConfirmationRegistrationForm);
     },
 
+    /**
+     * Removes elements of the Resend Verification interface.
+     */
     removeResendVerificationFeatures() {
-      console.log('Removing verification form features!')
       this.showSignInButton = false;
-      if (document.getElementById('resend-verification-message').innerHTML !== null) {
-        document.getElementById('resend-verification-message').innerHTML = '';
+      if (this.resendVerificationMessageElement.innerHTML !== null) {
+        this.resendVerificationMessageElement.innerHTML = Constants.EMPTY_STRING;
       }
       this.showResendVerificationCodeForm = false;
     }
