74dcf565900bd1f4c8b09282385076a1ca497e41
Wali Morris <walimmorris@gmail.com>
2022-11-03 14:06:34 -0700

    introduce constants that can be used across components in config.js and refactor code to use the vue :id binder across component form id attributes

--- config/config.js
+++ config/config.js
@@ -4,4 +4,26 @@ export default {
     region: 'us-west-2',
     clientId: '1kgfkcc6n3cnl8rqqi2ft66gce',
   },
+
+  FORM_ERROR_MESSAGES: {
+    INVALID_USERNAME: 'Invalid Username',
+    EMPTY_USERNAME: 'Empty Username',
+  },
+
+  FORM_SUCCESS_MESSAGES: {
+    CHECK_EMAIL: 'Check Email for Code',
+  },
+
+  COLOR: {
+    BLACK: '#000',
+    LIGHT_RED: '#E55451',
+  },
+
+  FONT_SIZE: {
+    SMALL: 'small',
+  },
+
+  FORM_VALUE: {
+    EMPTY: ''
+  }
 };
